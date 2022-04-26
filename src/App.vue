de0cf48dd6a352f041d824f3085eb0adf482678a
xcmk123 <vuan.learn@gmail.com>
2022-04-26 16:40:21 +0700

    new code

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<script>
+  import TodoComp from './components/TodoComp/index.vue'
+  let id = 0
+  export default {
+    components: {
+      TodoComp
+    },
+    data() {
+      return {
+        todoContent: '',
+        todos: [
+          { id: id++, content: `Content ${id}` },
+          { id: id++, content: `Content ${id}` },
+          { id: id++, content: `Content ${id}` },
+        ]
+      }
+    },
+    methods: {
+      output(id) {
+        console.log(id)
+      }
+    }
+  }
+</script>
+<template>
+  <input v-model="todoContent" placeholder="Type here" />
+  <div
+  >
+    <TodoComp 
+      v-for="todo in todos" 
+      :key="todo.id"
+      :id="todo.id"
+      :content="todo.content"
+      @output="output"
+    />
+  </div>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/TodoComp/index.vue
@@ -0,0 +1,26 @@
+<script>
+  export default {
+    props: {
+      content: String,
+      id: Number,
+    },
+    methods: {
+      onClick(id) {
+        this.$emit('output', id)
+      }
+    }
+  }
+</script>
+
+<template>
+  <div :id="id" @click="onClick(id)">
+    <p>
+      {{ content }}
+    </p>
+  </div>
+</template>
+
+
+<style>
+
+</style>
